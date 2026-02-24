ee597476e9d75566dec980ac869620f2ff62059a
gab1tod <gabintod@gmail.com>
2026-02-24 16:04:48 +0100

    add room covers

---
+++ scripts/components/fader.gd
@@ -0,0 +1,36 @@
+extends Node2D
+
+signal fading_out
+signal fading_in
+
+
+@export var fade_duration: float = 0.5
+@export var target: Node2D:
+	set(value):
+		if value:
+			target = value
+		else:
+			target = self
+
+
+func _ready() -> void:
+	if not target:
+		target = self
+	
+	if not target.visible:
+		target.modulate = Color.TRANSPARENT
+
+
+func fade_out() -> void:
+	fading_out.emit()
+	
+	var tween: Tween = get_tree().create_tween()
+	tween.tween_property(target, 'modulate', Color.TRANSPARENT, fade_duration)
+	tween.tween_callback(target.hide)
+
+func fade_in() -> void:
+	fading_in.emit()
+	
+	target.show()
+	var tween: Tween = get_tree().create_tween()
+	tween.tween_property(target, 'modulate', Color.WHITE, fade_duration)
